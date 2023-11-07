a4e2e9c726e08205eebc0f09567b1c0bf5a0f86f
mitchchan999 <mitchchan999@gmail.com>
2023-11-06 16:52:32 -0800

    fix return code

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -13,13 +13,13 @@ int main(int argc, char *argv[]) {
   if (argc != 3) {
     syslog(LOG_ERR, "Invalid number of arguments %d",argc);
     syslog(LOG_DEBUG, "Usage: %s <file> <string>", argv[0]);
-    return -1;
+    return 1;
   }
 
   fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
   if (fd == -1) {
     syslog(LOG_ERR, "Failed to open %s : %s", argv[1], strerror(errno));
-    return -1;
+    return 1;
   }
 
   syslog(LOG_DEBUG, "Writing %s to %s", argv[2], argv[1]);
@@ -28,7 +28,7 @@ int main(int argc, char *argv[]) {
 
   if (nbytes == -1) {
     syslog(LOG_ERR, "Failed to write %s : %s", argv[2], strerror(errno));
-    return -1;
+    return 1;
   }
 
   close(fd);
